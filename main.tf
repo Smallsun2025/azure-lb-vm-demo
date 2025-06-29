ccf848290833c833ba9cf052f5f49054e6d46b35
Smallsun2025 <lihongyang2025@gmail.com>
2025-06-29 20:53:43 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -117,5 +117,24 @@ resource "azurerm_lb_backend_address_pool" "lb_backend_pool" {
   resource_group_name = azurerm_resource_group.rg.name
   loadbalancer_id     = azurerm_lb.lb.id
 }
+resource "azurerm_lb_nat_rule" "rdp_rule_vm1" {
+  name                           = "rdp-vm1"
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 50001
+  backend_port                   = 3389
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azurerm_lb_nat_rule" "rdp_rule_vm2" {
+  name                           = "rdp-vm2"
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 50002
+  backend_port                   = 3389
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
 
 
