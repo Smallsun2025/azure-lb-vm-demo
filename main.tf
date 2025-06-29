d600356122ec92e0c790f13fcdd2c90f95d9e81f
Smallsun2025 <lihongyang2025@gmail.com>
2025-06-29 20:56:27 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -137,4 +137,80 @@ resource "azurerm_lb_nat_rule" "rdp_rule_vm2" {
   frontend_ip_configuration_name = "PublicIPAddress"
 }
 
+# NIC for VM1
+resource "azurerm_network_interface" "nic_vm1" {
+  name                = "nic-vm1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    load_balancer_backend_address_pools_ids = [azurerm_lb_backend_address_pool.lb_backend_pool.id]
+    load_balancer_inbound_nat_rules_ids     = [azurerm_lb_nat_rule.rdp_rule_vm1.id]
+  }
+}
+
+# NIC for VM2
+resource "azurerm_network_interface" "nic_vm2" {
+  name                = "nic-vm2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    load_balancer_backend_address_pools_ids = [azurerm_lb_backend_address_pool.lb_backend_pool.id]
+    load_balancer_inbound_nat_rules_ids     = [azurerm_lb_nat_rule.rdp_rule_vm2.id]
+  }
+}
+resource "azurerm_windows_virtual_machine" "vm1" {
+  name                = "vm1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [azurerm_network_interface.nic_vm1.id]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter"
+    version   = "latest"
+  }
+
+  provision_vm_agent = true
+  custom_data        = base64encode(file("startup-script.ps1"))
+}
+
+resource "azurerm_windows_virtual_machine" "vm2" {
+  name                = "vm2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [azurerm_network_interface.nic_vm2.id]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter"
+    version   = "latest"
+  }
+
+  provision_vm_agent = true
+  custom_data        = base64encode(file("startup-script.ps1"))
+}
 
